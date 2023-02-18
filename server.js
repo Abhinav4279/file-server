e571b69aea670d16f52111a80dafc150057299c3
Abhinav Sharma <54229503+Abhinav4279@users.noreply.github.com>
2023-02-18 17:04:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,27 +6,29 @@ var fs = require("fs");
         context.fail("writeFile failed: " + err);
     } else {
         context.succeed("writeFile succeeded");
+     
+        // See https://github.com/typicode/json-server#module
+        const jsonServer = require('json-server')
+        const server = jsonServer.create()
+        const router = jsonServer.router('/tmp/db.json')
+        const middlewares = jsonServer.defaults()
+
+        server.use(middlewares)
+        // Add this before server.use(router)
+        server.use(jsonServer.rewriter({
+            '/api/*': '/$1',
+            '/blog/:resource/:id/show': '/:resource/:id'
+        }))
+        server.use(router)
+        server.listen(3000, () => {
+            console.log('JSON Server is running')
+        })
+
+        // Export the Server API
+        module.exports = server
     }
   });
  };
 
 createFile();
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('/tmp/db.json')
-const middlewares = jsonServer.defaults()
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
-
-// Export the Server API
-module.exports = server
